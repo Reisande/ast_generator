46b7bdf9a2eb4b7d08554ebb4fd1fe9ab207b1ac
Reisande <mryabk2@uic.edu>
2019-09-10 19:12:47 -0500

    Initial commit. No code, just a description of hwo this will work

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+
+// simple ast generator
+// takes an input of a file name of a scheme file, and generates an output
+// ast in the form of an ascii tree through recursive descent
+
+// Also, I would like to expand this into a REPL like command line
+// utility, where you can add multiple commands incrementally and ahve direct
+// output to the command line
+int main() {
+
+}
